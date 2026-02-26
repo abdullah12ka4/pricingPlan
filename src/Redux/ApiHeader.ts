bd394fd3e9faa92bfbd41ed0e87d509c4cf7fb21
abdullah12ka4 <itsjackabdullah25@gmail.com>
2026-02-26 12:30:06 +0500

    initial

--- src/Redux/ApiHeader.ts
+++ src/Redux/ApiHeader.ts
@@ -3,8 +3,7 @@ import Cookies from "js-cookie";
 
 
 export const header = fetchBaseQuery({ 
-    // baseUrl: 'https://barely-jokes-involve-apr.trycloudflare.com',
-    baseUrl: "http://192.168.1.39:3000",
+    baseUrl: process.env.NEXT_PUBLIC_API_URL || "http://192.168.1.39:3000",
     credentials: "include",
     prepareHeaders: (headers) => {
       const token = Cookies.get("accessToken");
